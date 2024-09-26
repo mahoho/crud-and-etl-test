39215f03081182d0106ce915c56037d0dd500c3e
Oleksii R <o.ratnykov@gmail.com>
2024-09-26 20:29:05 +0200

    minor fixes

--- app/helpers.php
+++ app/helpers.php
@@ -6,22 +6,25 @@
  * @param string $field
  * @return string
  */
-function prepareFieldName(string $field): string {
-    // remove quotes and replace simple chars with underscores
-    $preparedField = trim(str_replace([
-        ' ',
-        '.',
-        '(',
-        ')'
-    ], '_', $field), " \n\r\t\v\0_\"'");
 
-    // remove non-alpha numeric chars
-    $preparedField = preg_replace("/[^\w]/", '', $preparedField);
+if(!function_exists('prepareFieldName')) {
+    function prepareFieldName(string $field): string {
+        // remove quotes and replace simple chars with underscores
+        $preparedField = trim(str_replace([
+            ' ',
+            '.',
+            '(',
+            ')'
+        ], '_', $field), " \n\r\t\v\0_\"'");
 
-    // make sure name does start with letter:
-    $preparedField = preg_replace("/^\d+/", '', $preparedField);
+        // remove non-alpha numeric chars
+        $preparedField = preg_replace("/[^\w]/", '', $preparedField);
 
-    return strtolower($preparedField);
+        // make sure name does start with letter:
+        $preparedField = preg_replace("/^\d+/", '', $preparedField);
+
+        return strtolower($preparedField);
+    }
 }
 
 /**
@@ -30,27 +33,30 @@ function prepareFieldName(string $field): string {
  * @param string $text
  * @return string
  */
-function cleanTextFileContent(string $text) : string {
-    if (!$text) {
-        return '';
+
+if(!function_exists('cleanTextFileContent')) {
+    function cleanTextFileContent(string $text) : string {
+        if (!$text) {
+            return '';
+        }
+
+        // we need plain text, not html entities
+        $text = html_entity_decode($text);
+
+        // remove bom
+        $bom = pack('H*','EFBBBF');
+        $text = preg_replace("/^$bom/", '', $text);
+
+        // remove non-printable characters
+        $text = preg_replace('/[^[:print:]\r\n]/', '', $text);
+
+        // remove broken utf-8 symbols
+        $encoding = mb_detect_encoding($text, mb_detect_order());
+
+        if($encoding === "UTF-8") {
+            $text = mb_convert_encoding($text, 'UTF-8', 'UTF-8');
+        }
+
+        return iconv($encoding, "UTF-8//IGNORE", $text) ?: '';
     }
-
-    // we need plain text, not html entities
-    $text = html_entity_decode($text);
-
-    // remove bom
-    $bom = pack('H*','EFBBBF');
-    $text = preg_replace("/^$bom/", '', $text);
-
-    // remove non-printable characters
-    $text = preg_replace('/[^[:print:]\r\n]/', '', $text);
-
-    // remove broken utf-8 symbols
-    $encoding = mb_detect_encoding($text, mb_detect_order());
-
-    if($encoding === "UTF-8") {
-        $text = mb_convert_encoding($text, 'UTF-8', 'UTF-8');
-    }
-
-    return iconv($encoding, "UTF-8//IGNORE", $text) ?: '';
 }
